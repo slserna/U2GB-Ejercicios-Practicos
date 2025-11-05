d4e5448c2909a2369f2aabb65f68c135c9fe9833
slserna <1224100715.slsr@gmail.com>
2025-11-04 21:48:59 -0600

    Update Invertir.java

--- U2GB Ejercicios Prácticos/EjerciciosPilas/Invertir.java
+++ U2GB Ejercicios Prácticos/EjerciciosPilas/Invertir.java
@@ -5,38 +5,53 @@
 package Pilas;
 
 /**
- *
- @author 12241
- * @Autor Sara Lizbeth Serna Rodriguez
+ * @author 12241
+ * @Autor Sara Lizbeth Serna Rodríguez
  * Grupo: GTID0141
+ * Fecha: 30/10/25
  * EJERCICIO DE PILA -----
- * 30/10/25
  */
-import java.util.Stack;
+
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * Ejercicio 4: Invertir una palabra.
  * 
  * Objetivo:
- * - Usar una pila de caracteres para invertir una palabra.
+ * - Utilizar una pila genérica para invertir una palabra carácter por carácter.
+ * 
+ * Descripción:
+ * El programa solicita al usuario una palabra, almacena cada carácter en una pila
+ * (estructura LIFO) y luego los extrae para mostrarlos en orden inverso.
+ * 
+ * Parametrización:
+ * - Se usa Stack<T> con tipo T = Character.
  */
-public class Invertir {
+
+public class Invertir<T> {
+
+    /**
+     * Método principal que ejecuta la inversión de una palabra.
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Stack<Character> pila = new Stack<>();
+
         System.out.print("Ingrese una palabra: ");
         String palabra = sc.nextLine();
 
-        Stack<Character> pila = new Stack<>();
+        // Insertar cada carácter en la pila
         for (char c : palabra.toCharArray()) {
             pila.push(c);
         }
 
+        // Mostrar la palabra invertida
         System.out.print("Invertida: ");
         while (!pila.isEmpty()) {
             System.out.print(pila.pop());
         }
+
+        sc.close();
     }
 }
-// Entrada: UTNG → Salida: GNTU
-

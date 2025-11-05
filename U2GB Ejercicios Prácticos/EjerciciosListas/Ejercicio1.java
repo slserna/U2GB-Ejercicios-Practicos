b7e5e5fe82ce39fed014bb66fcf55b8b439253d4
slserna <1224100715.slsr@gmail.com>
2025-11-04 21:33:17 -0600

    Update Ejercicio1.java

--- U2GB Ejercicios Prácticos/EjerciciosListas/Ejercicio1.java
+++ U2GB Ejercicios Prácticos/EjerciciosListas/Ejercicio1.java
@@ -8,82 +8,109 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
- *
- * @author 12241
- * @Autor Sara Lizbeth Serna Rodriguez
- * Grupo: GTID0141
- * EJERCICIO 1 -----
+ * Clase: ListaEnlazada<T>
  * 
+ * Objetivo:
+ * - Implementar una lista enlazada simple genérica.
+ * - Permitir insertar elementos, mostrarlos y eliminar aquellos mayores a un valor dado.
+ * 
+ * Autor: Sara Lizbeth Serna Rodríguez
+ * Grupo: GTID0141
  */
+public class ListaEnlazada<T extends Comparable<T>> {
 
+    /** Nodo interno con encapsulamiento */
+    private class Nodo {
+        private T dato;
+        private Nodo siguiente;
 
-public class Ejercicio1 {
-    Nodo cabeza;
-    
-    class Nodo {
-    int dato;
-    Nodo siguiente;
+        public Nodo(T dato) {
+            this.dato = dato;
+            this.siguiente = null;
+        }
 
-    public Nodo(int dato) {
-        this.dato = dato;
-        this.siguiente = null;
+        public T getDato() {
+            return dato;
+        }
+
+        public Nodo getSiguiente() {
+            return siguiente;
+        }
+
+        public void setSiguiente(Nodo siguiente) {
+            this.siguiente = siguiente;
+        }
     }
-}
 
-    // Insertar al final
-    void insertarFinal(int valor) {
+    /** Referencia al primer nodo de la lista */
+    private Nodo cabeza;
+
+    /** Inserta un elemento al final de la lista */
+    public void insertarFinal(T valor) {
         Nodo nuevo = new Nodo(valor);
         if (cabeza == null) {
             cabeza = nuevo;
         } else {
             Nodo aux = cabeza;
-            while (aux.siguiente != null)
-                aux = aux.siguiente;
-            aux.siguiente = nuevo;
+            while (aux.getSiguiente() != null) {
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(nuevo);
         }
     }
 
-    // Mostrar lista
-    void mostrarLista() {
+    /** Muestra el contenido de la lista en consola */
+    public void mostrarLista() {
         Nodo aux = cabeza;
         while (aux != null) {
-            System.out.print(aux.dato + " -> ");
-            aux = aux.siguiente;
+            System.out.print(aux.getDato() + " -> ");
+            aux = aux.getSiguiente();
         }
         System.out.println("null");
     }
 
-    // Eliminar mayores a un valor dado
-    void eliminarMayores(int limite) {
-        while (cabeza != null && cabeza.dato > limite)
-            cabeza = cabeza.siguiente;
+    /**
+     * Elimina los nodos cuyos valores sean mayores que el límite dado.
+     * 
+     * @param limite valor de referencia para eliminar elementos mayores
+     */
+    public void eliminarMayores(T limite) {
+        while (cabeza != null && cabeza.getDato().compareTo(limite) > 0) {
+            cabeza = cabeza.getSiguiente();
+        }
 
         Nodo actual = cabeza;
-        while (actual != null && actual.siguiente != null) {
-            if (actual.siguiente.dato > limite)
-                actual.siguiente = actual.siguiente.siguiente;
-            else
-                actual = actual.siguiente;
+        while (actual != null && actual.getSiguiente() != null) {
+            if (actual.getSiguiente().getDato().compareTo(limite) > 0) {
+                actual.setSiguiente(actual.getSiguiente().getSiguiente());
+            } else {
+                actual = actual.getSiguiente();
+            }
         }
     }
 
+    /** Método principal para probar la lista enlazada */
     public static void main(String[] args) {
-        Ejercicio1 lista = new Ejercicio1();
+        ListaEnlazada<Integer> lista = new ListaEnlazada<>();
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
 
-        // Generar números aleatorios
-        for (int i = 0; i < 10; i++)
+        // Generar y agregar 10 números aleatorios
+        for (int i = 0; i < 10; i++) {
             lista.insertarFinal(rand.nextInt(100) + 1);
+        }
 
-        System.out.println("Lista original:");
+        System.out.println("\n Lista original:");
         lista.mostrarLista();
 
-        System.out.print("Ingresa un valor límite: ");
+        System.out.print("\nIngrese un valor límite: ");
         int limite = sc.nextInt();
 
         lista.eliminarMayores(limite);
-        System.out.println("Lista después de eliminar mayores a " + limite + ":");
+        System.out.println("\n Lista después de eliminar elementos mayores a " + limite + ":");
         lista.mostrarLista();
+
+        sc.close();
     }
 }
+
